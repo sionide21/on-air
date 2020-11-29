840337fbd8bbb41efc12e5bc6ebf76e30ebce66c
Ben Olive <sionide21@gmail.com>
2020-11-28 22:24:12 -0500

    mix nerves.new firmware --module OnAir --app on_air --target rpi0

---
+++ firmware/lib/on_air.ex
@@ -0,0 +1,18 @@
+defmodule OnAir do
+  @moduledoc """
+  Documentation for OnAir.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> OnAir.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ firmware/test/on_air_test.exs
@@ -0,0 +1,8 @@
+defmodule OnAirTest do
+  use ExUnit.Case
+  doctest OnAir
+
+  test "greets the world" do
+    assert OnAir.hello() == :world
+  end
+end
