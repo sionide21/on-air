7026e7b2efd9aa5a6e78ffe6aab4064de7a56e92
Ben Olive <sionide21@gmail.com>
2020-12-04 22:07:23 -0500

    Add circuits dep

--- firmware/mix.exs
+++ firmware/mix.exs
@@ -35,6 +35,7 @@ defmodule OnAir.MixProject do
       {:shoehorn, "~> 0.7.0"},
       {:ring_logger, "~> 0.8.1"},
       {:toolshed, "~> 0.2.13"},
+      {:circuits_i2c, "~> 0.1"},
 
       # Dependencies for all targets except :host
       {:nerves_runtime, "~> 0.11.3", targets: @all_targets},
